501add688a70b07251669bf13b7fff6edea4799d
Adrian Janczyk <adrian.turok@gmail.com>
2026-01-05 16:44:34 +0100

    Implemented light mode and dark mode icons, applied some small fixes added icons and tooltips for wired and charging

--- src/keychron_widget.py
+++ src/keychron_widget.py
@@ -1,7 +1,7 @@
 import hid
 import sys
 import os
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer,Qt
 from PyQt6.QtWidgets import (QApplication
                              ,QSystemTrayIcon
                              ,QMenu
@@ -21,19 +21,15 @@ class MainWindow(QSystemTrayIcon):
     def __init__(self,appin):
         super().__init__(appin)
 
+        self.set_icons_scheme(app.styleHints().colorScheme())
+
+
         self.menu = QMenu()
         self.quit = QAction("Quit")
         self.quit.triggered.connect(appin.quit)
         self.menu.addAction(self.quit)
 
         self.setContextMenu(self.menu)
-
-        self.icon0 = QIcon(path_to_main + '/battery0.png')
-        self.icon1 = QIcon(path_to_main + '/battery1.png')
-        self.icon2 = QIcon(path_to_main + '/battery2.png')
-        self.icon3 = QIcon(path_to_main + '/battery3.png')
-        self.icon4 = QIcon(path_to_main + '/battery4.png')
-        self.icon5 = QIcon(path_to_main + '/battery5.png')
             
         self.update_status()
         self.setVisible(True)
@@ -42,6 +38,29 @@ class MainWindow(QSystemTrayIcon):
         self.timer.setInterval(300000)
         self.timer.timeout.connect(self.update_status)
         self.timer.start()
+    
+    def set_icons_scheme(self,scheme_color:Qt.ColorScheme):
+        print(scheme_color)
+        if scheme_color == Qt.ColorScheme.Light:
+            self.icon0 = QIcon(path_to_main + '/battery0White.png')
+            self.icon1 = QIcon(path_to_main + '/battery1White.png')
+            self.icon2 = QIcon(path_to_main + '/battery2White.png')
+            self.icon3 = QIcon(path_to_main + '/battery3White.png')
+            self.icon4 = QIcon(path_to_main + '/battery4White.png')
+            self.icon5 = QIcon(path_to_main + '/battery5White.png')
+            self.icon_wired = QIcon(path_to_main + '/usbiconWhite.png')   
+            self.charging = QIcon(path_to_main + '/charging_boltWhite.png')               
+        else:
+            self.icon0 = QIcon(path_to_main + '/battery0.png')
+            self.icon1 = QIcon(path_to_main + '/battery1.png')
+            self.icon2 = QIcon(path_to_main + '/battery2.png')
+            self.icon3 = QIcon(path_to_main + '/battery3.png')
+            self.icon4 = QIcon(path_to_main + '/battery4.png')
+            self.icon5 = QIcon(path_to_main + '/battery5.png')
+            self.icon_wired = QIcon(path_to_main + '/usbicon.png')
+            self.charging = QIcon(path_to_main + '/charging_bolt.png')     
+        
+        self.update_status()
 
     def set_battery_icon(self):
         if self.battery_level:
@@ -71,10 +90,16 @@ class MainWindow(QSystemTrayIcon):
                 data_array = [hex_data[x:x+2] for x in range(0,len(hex_data),2) ]
 
                 self.battery_level = int(data_array[20],16)
-                self.setToolTip(f'Battery level:{self.battery_level}%')
+                if self.battery_level > 100:
+                    self.setToolTip('Battery is currently charging')
+                    self.setIcon(self.charging)
+                else:
+                    self.setToolTip(f'Battery level:{self.battery_level}%')
+                    self.set_battery_icon()
         except hid.HIDException:
             if len(hid.enumerate(vid=0x3434, pid=0xd048))>0:
-                self.setToolTip('Device dose not report battery level while wired')
+                self.setToolTip('Wired Connection')
+                self.setIcon(self.icon_wired)
             else:
                 self.setToolTip('Device couldnt be detected')
             self.battery_level = None
@@ -82,7 +107,6 @@ class MainWindow(QSystemTrayIcon):
     def update_status(self):
         try:
             self.get_keychron_battery_status()    
-            self.set_battery_icon()
         except IndexError:
             print('unable to load data')
             
@@ -91,6 +115,9 @@ class MainWindow(QSystemTrayIcon):
 app = QApplication(sys.argv)
 
 w = MainWindow(app)
+
+app.styleHints().colorSchemeChanged.connect(w.set_icons_scheme)
+
 w.show()
 
 app.exec()
